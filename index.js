14dc326e5ecfeb3b3d5e6918c3990bb375158db3
DESKTOP-7I51HR0\Nuttyld <nutt_songsaksri@hotmail.com>
2020-12-11 17:25:25 +0700

    ลองใช้ function 2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let x = 2;
 let y = "nutty";
-let z ;
+let z;
 let w = null;
 let k = {
-    ds: "adkd",
-    ff: 2
+  ds: "adkd",
+  ff: 2,
+};
+
+function a(x, z) {
+  return x + z;
 }
 
 console.log(typeof x);
@@ -14,3 +18,4 @@ console.log(typeof w);
 console.log(typeof k);
 console.log(typeof k.ds);
 console.log(typeof k.ff);
+console.log(a(x, k.ff));
