b7acbffd7823fc91d7e04c30352e0fcb05bb0058
DESKTOP-7I51HR0\Nuttyld <nutt_songsaksri@hotmail.com>
2020-12-11 17:43:28 +0700

    เรื่อง อาเรย์

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-let x = 2;
-let y = "nutty";
-let z;
-let w = null;
-let k = {
-  ds: "adkd",
-  ff: 2,
-};
+//เรื่อง typeof
+// let x = 2;
+// let y = "nutty";
+// let z;
+// let w = null;
+// let k = {
+//   ds: "adkd",
+//   ff: 2,
+// };
 
-function a(x, z) {
-  return x + z;
-}
+// function a(x, z) {
+//   return x + z;
+// }
 
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
-console.log(typeof w);
-console.log(typeof k);
-console.log(typeof k.ds);
-console.log(typeof k.ff);
-console.log(a(x, k.ff));
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+// console.log(typeof w);
+// console.log(typeof k);
+// console.log(typeof k.ds);
+// console.log(typeof k.ff);
+// console.log(a(x, k.ff));
+//---------------------------------------------------------------
+
+//เรื่อง Array
+var c = [1, 2, 3];
+
+console.log(c[0]);
+console.log(c.length); //จำนวน
+console.log(
+  c.sort((a, b) => {
+    return b - a;
+  })
+);
